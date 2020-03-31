04b665e57c7e8c5a84707c985060d2e4c6bee37b
Luke Dary <ldary@redhat.com>
2020-03-30 22:12:32 -0400

    Cleanup and Dashboard

--- dashboard/Bank/Bank.gd
+++ dashboard/Bank/Bank.gd
@@ -1,34 +1,32 @@
-extends MeshInstance
+extends Area2D
 
-enum LANG {Headquarters, English, Spanish, Japanese};
+enum BLDG {Headquarters, Bank, Edge};
+onready var routed = preload("res://Bank/on.png")
+onready var unrouted = preload("res://Bank/off.png")
+onready var bankAnimation = preload("res://Bank/BankAnimation.tres")
+onready var hqAnimation = preload("res://Bank/HQAnimation.tres")
 
-onready var nav = get_node("/root/Dashboard/Nav")
-onready var packet = preload("res://Packet/Packet.tscn")
-onready var bldg_hq = preload("res://Bank/bldg_hq.tres")
-onready var bldg_en = preload("res://Bank/bldg_en.tres")
-onready var bldg_es = preload("res://Bank/bldg_es.tres")
-onready var bldg_jp = preload("res://Bank/bldg_jp.tres")
-
-export(LANG) var Locale = LANG.English;
-export var light_color: Color = Color('#13d3d7');
-export var Flow: bool = false;
-export var SpawnPos:Vector3;
 const MAX = 10;
-var rot = 0.0;
-var t = 0.0;
-var locales = ["HQ","EN","ES","JP","FR","SR"]
-var buildings = [];
 var rng = RandomNumberGenerator.new()
+var built = false
+export var BankName = "Bank Name"
+export var Network = false
+export(BLDG) var Building = BLDG.Bank;
+
 
 func _ready():
+	$Label/LabelBG/Name.text = BankName
+	match Building:
+		0:
+			$Sprite.frames = hqAnimation
+			$Label.margin_top = -227
+			$Label.margin_bottom = -144
+		1:
+			$Sprite.frames = bankAnimation
+			$Label.margin_top = -185
+			$Label.margin_bottom = -103
+	
 #	connect("input_event", self, "on_input_event")
-	#$OmniLight.light_color = light_color
-	$DataPipe.get_surface_material(0).albedo_color = Color(light_color.r, light_color.g, light_color.b, 0.05)
-	$DataPipe.get_surface_material(0).emission = Color(light_color.r, light_color.g, light_color.b, 0.05)
-	$DataBits.draw_pass_1.material.emission = light_color
-	SpawnPos = to_global($Spawn.transform.origin)
-	buildings = [bldg_hq, bldg_en, bldg_es, bldg_jp]
-	self.mesh = self.buildings[self.Locale];
 
 #func on_input_event(camera, event, click_position, click_normal, shape_idx):
 #	var mouse_click = event as InputEventMouseButton
@@ -38,32 +36,43 @@ func _ready():
 		#self.Locale = rng.randi_range(0,2)
 
 func _physics_process(delta):
-	pass
+	if Network:
+		$Label/LabelBG/Connect.texture = routed
+	else:
+		$Label/LabelBG/Connect.texture = unrouted
+
+func build():
+	if $Sprite.frame < 24:
+		$Sprite.play("build")
+	else:
+		$Sprite.play("build", true)
 
 func toggle_Data():
-	if $Data.is_stopped():
-		$Data.start()
-		$OmniLight.visible = true
-		$DataBits.emitting = true
-		$DataPipe.visible = true
-		$DataBits.draw_pass_1.material.emission = light_color
-	else:
-		$Data.stop()
-		$OmniLight.visible = false
-		$DataBits.emitting = false
-		$DataPipe.visible = false
-	
+	pass
+#	if $Data.is_stopped():
+#		$Data.start()
+#		$OmniLight.visible = true
+#		$DataBits.emitting = true
+#		$DataPipe.visible = true
+#		$DataBits.draw_pass_1.material.emission = light_color
+#	else:
+#		$Data.stop()
+#		$OmniLight.visible = false
+#		$DataBits.emitting = false
+#		$DataPipe.visible = false
+#
 
 func spawn_packet(dest):
-	var destination = get_parent().find_node(dest)
-	if destination != self:
-		var bit = packet.instance()
-		bit.transform.origin = SpawnPos
-		rng.randomize()
-#		bit.light_color = Color(rng.randf_range(0.1, 1.0),rng.randf_range(0.1, 1.0),rng.randf_range(0.1, 1.0))
-		bit.light_color = Color('13d3d7')
-		nav.add_child(bit)
-		bit.move_to(destination.SpawnPos)
+	pass
+#	var destination = get_parent().find_node(dest)
+#	if destination != self:
+#		var bit = packet.instance()
+#		bit.transform.origin = SpawnPos
+#		rng.randomize()
+##		bit.light_color = Color(rng.randf_range(0.1, 1.0),rng.randf_range(0.1, 1.0),rng.randf_range(0.1, 1.0))
+#		bit.light_color = Color('13d3d7')
+#		nav.add_child(bit)
+#		bit.move_to(destination.SpawnPos)
 
 
 func _on_Data_timeout():
@@ -71,3 +80,11 @@ func _on_Data_timeout():
 	#$OmniLight.light_color = Color(rng.randf_range(0.1, 1.0),rng.randf_range(0.1, 1.0),rng.randf_range(0.1, 1.0))
 	spawn_packet("HQ")
 	#spawn_packet(locales[rng.randi_range(0,4)])
+
+
+func _on_Sprite_animation_finished():
+	$Label.visible = !$Label.visible
+	if $Label.visible:
+		$Label/LabelBG/Connect.texture = routed
+	else:
+		$Label/LabelBG/Connect.texture = unrouted
--- dashboard/BankDashboard.gd
+++ dashboard/BankDashboard.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-var wide_zoom = Vector2(2.5, 2.5)
-var narrow_zoom = Vector2(0.5,0.5)
+var wide_zoom = Vector2(3.0, 3.0)
+var narrow_zoom = Vector2(0.75,0.75)
 var zoom_time = 1
 var move_time = 1
 var scene = 0
@@ -15,22 +15,6 @@ func _input(event):
 	if event is InputEventKey:
 		if event.pressed and event.scancode == KEY_ESCAPE:
 			get_tree().quit()
-		if event.pressed and event.scancode == KEY_1:
-			pass
-		if event.pressed and event.scancode == KEY_2:
-			$HQ.play("build",true)
-		if event.pressed and event.scancode == KEY_3:
-			$London.play("build")
-		if event.pressed and event.scancode == KEY_4:
-			$London.play("build",true)
-		if event.pressed and event.scancode == KEY_SPACE:
-			if $FocalPoint/Cam.zoom == wide_zoom:
-				$Tween.interpolate_property($FocalPoint/Cam, "zoom", wide_zoom, narrow_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-			else:
-				$Tween.interpolate_property($FocalPoint/Cam, "zoom", narrow_zoom, wide_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-			$Tween.start()
-		if event.pressed and event.scancode == KEY_X:
-			pass
 		if event.pressed and event.scancode == KEY_RIGHT:
 			if(scene < 10):
 				scene = scene + 1
@@ -39,50 +23,73 @@ func _input(event):
 			if(scene > 0):
 				scene = scene - 1
 				run_scene(scene, true)
+#		if event.pressed and event.scancode == KEY_1:
+#			pass
+#		if event.pressed and event.scancode == KEY_2:
+#			$HQ.play("build",true)
+#		if event.pressed and event.scancode == KEY_3:
+#			$London.play("build")
+#		if event.pressed and event.scancode == KEY_4:
+#			$London.play("build",true)
+#		if event.pressed and event.scancode == KEY_SPACE:
+#			if $FocalPoint/Cam.zoom == wide_zoom:
+#				$Tween.interpolate_property($FocalPoint/Cam, "zoom", wide_zoom, narrow_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+#			else:
+#				$Tween.interpolate_property($FocalPoint/Cam, "zoom", narrow_zoom, wide_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+#			$Tween.start()
+#		if event.pressed and event.scancode == KEY_X:
+#			pass
+#		
 
 func run_scene(scene,reverse):
 	match scene:
 		0:
 			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $CenterPoint.position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 			if reverse:
-				$HQ.play("build",true)
+				$HQ.build()
 				$Tween.interpolate_property($FocalPoint/Cam, "zoom", $FocalPoint/Cam.zoom, wide_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 			$Tween.start()
 		1:
 			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $HQ.position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 			$Tween.interpolate_property($FocalPoint/Cam, "zoom", $FocalPoint/Cam.zoom, narrow_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 			$Tween.start()
-			$HQ.play("build")
+			$HQ.build()
+			$HQ.Network = true
 			if reverse:
-				$London.play("build",true)
+				$London.build()
 		2:
 			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $London.position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 			$Tween.start()
-			$London.play("build")
+			$London.build()
+			$London.Network = true
 			if reverse:
-				$Sydney.play("build",true)
+				$Sydney.build()
 		3:
 			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $Sydney.position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 			$Tween.start()
-			$Sydney.play("build")
+			$Sydney.build()
+			$Sydney.Network = true
 			if reverse:
-				$Singapore.play("build",true)
+				$Singapore.build()
 		4:
 			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $Singapore.position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 			$Tween.start()
-			$Singapore.play("build")
+			$Singapore.build()
+			$Singapore.Network = true
 			if reverse:
-				$SanFrancisco.play("build",true)
+				$SanFrancisco.build()
 		5:
 			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $SanFrancisco.position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 			$Tween.start()
-			$SanFrancisco.play("build")
+			$SanFrancisco.build()
+			$SanFrancisco.Network = true
 			if reverse:
-				$SaoPaulo.play("build",true)
+				$SaoPaulo.build()
 		6:
 			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $SaoPaulo.position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 			$Tween.start()
-			$SaoPaulo.play("build")
+			$SaoPaulo.build()
+			$SaoPaulo.Network = true
 			if reverse:
 				$Tween.interpolate_property($FocalPoint/Cam, "zoom", $FocalPoint/Cam.zoom, narrow_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 		7:
@@ -91,18 +98,19 @@ func run_scene(scene,reverse):
 				$Tween.interpolate_property($FocalPoint/Cam, "zoom", $FocalPoint/Cam.zoom, wide_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 				$Tween.start()
 			if reverse:
-				$Frankfurt.play("build",true)
+				$Frankfurt.build()
 		8:
 			if !reverse:
 				$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $Frankfurt.position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 				$Tween.interpolate_property($FocalPoint/Cam, "zoom", $FocalPoint/Cam.zoom, narrow_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 				$Tween.start()
-				$Frankfurt.play("build")	
+				$Frankfurt.build()
+				$Frankfurt.Network = true
 			if reverse:
-				$London.play("build")
+				$London.Network = true
 		9:
 			if !reverse:
-				$London.play("build", true)
+				$London.Network = false
 			if reverse:
 				$Tween.interpolate_property($FocalPoint/Cam, "zoom", $FocalPoint/Cam.zoom, narrow_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 		10:
