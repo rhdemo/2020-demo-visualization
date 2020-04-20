19f4df96c4c0bba0f726549cc8d30f98b25261f0
Luke Dary <ldary@redhat.com>
2020-04-20 09:19:08 -0400

    Folder reorg

---
+++ dashboard/src/dashboard/BankDashboard.gd
@@ -0,0 +1,145 @@
+extends Node2D
+
+var wide_zoom = Vector2(3.0, 3.0)
+var narrow_zoom = Vector2(.9,.9)
+var zoom_time = 1
+var move_time = 1
+var edge_scale = 0.35
+var scene = 0
+var ldnEdges
+
+
+#WS URL ws://ui-admin-hq.apps.summit-hq1.openshift.redhatkeynote.com/socket
+
+func _ready():
+	 ldnEdges = $HQ/Banks/London/Edges.get_children()
+
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_ESCAPE:
+			get_tree().quit()
+		if event.pressed and event.scancode == KEY_RIGHT:
+			if(scene < 12):
+				scene = scene + 1
+				run_scene(scene, false)
+		if event.pressed and event.scancode == KEY_LEFT:
+			if(scene > 0):
+				scene = scene - 1
+				run_scene(scene, true)
+		if event.pressed and event.scancode == KEY_UP:
+			for edge in $HQ/Edges.get_children():
+				$Tween.interpolate_property(edge.get_node("Sprite"), "scale", edge.get_node("Sprite").scale, Vector2(1.0,1.0), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			for bank in $HQ/Banks.get_children():
+				for edge in bank.get_node("Edges").get_children():
+					$Tween.interpolate_property(edge.get_node("Sprite"), "scale", edge.get_node("Sprite").scale, Vector2(1.0,1.0), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.start()
+		if event.pressed and event.scancode == KEY_DOWN:
+			for edge in $HQ/Edges.get_children():
+				$Tween.interpolate_property(edge.get_node("Sprite"), "scale", edge.get_node("Sprite").scale, Vector2(edge_scale,edge_scale), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			for bank in $HQ/Banks.get_children():
+				for edge in bank.get_node("Edges").get_children():
+					$Tween.interpolate_property(edge.get_node("Sprite"), "scale", edge.get_node("Sprite").scale, Vector2(edge_scale,edge_scale), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.start()
+#		if event.pressed and event.scancode == KEY_1:
+#			pass
+#		if event.pressed and event.scancode == KEY_2:
+#			$HQ.play("build",true)
+#		if event.pressed and event.scancode == KEY_3:
+#			$London.play("build")
+#		if event.pressed and event.scancode == KEY_4:
+#			$London.play("build",true)
+#		if event.pressed and event.scancode == KEY_X:
+#			pass
+#		
+
+func run_scene(scene,reverse):
+	match scene:
+		0: #Opening Global Map
+			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $CenterPoint.position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			if reverse:
+				$HQ.build(reverse)
+				$Tween.interpolate_property($FocalPoint/Cam, "zoom", $FocalPoint/Cam.zoom, wide_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.start()
+		1: #Zoom to HQ
+			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $HQ.global_position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.interpolate_property($FocalPoint/Cam, "zoom", $FocalPoint/Cam.zoom, narrow_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.start()
+			if !reverse:
+				$HQ.build()
+				$HQ.Network = true
+			else:
+				$HQ/Banks/London.build(reverse)
+		2: #Pan to London
+			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $HQ/Banks/London.global_position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.start()
+			if !reverse:
+				$HQ/Banks/London.build()
+				$HQ/Banks/London.Network = true
+			else:
+				$HQ/Banks/Sydney.build(reverse)
+		3: #Pan to Sydney
+			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $HQ/Banks/Sydney.global_position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.start()
+			if !reverse:
+				$HQ/Banks/Sydney.build()
+				$HQ/Banks/Sydney.Network = true
+			else:
+				$HQ/Banks/Singapore.build(reverse)
+		4: #Pan to Singapore
+			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $HQ/Banks/Singapore.global_position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.start()
+			if !reverse:
+				$HQ/Banks/Singapore.build()
+				$HQ/Banks/Singapore.Network = true
+			else:
+				$HQ/Banks/SanFrancisco.build(reverse)
+		5: #Pan to SF
+			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $HQ/Banks/SanFrancisco.global_position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.start()
+			if !reverse:
+				$HQ/Banks/SanFrancisco.build()
+				$HQ/Banks/SanFrancisco.Network = true
+			else:
+				$HQ/Banks/SaoPaulo.build(reverse)
+		6: #Pan to SaoPaulo
+			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $HQ/Banks/SaoPaulo.global_position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.start()
+			if !reverse:
+				$HQ/Banks/SaoPaulo.build()
+				$HQ/Banks/SaoPaulo.Network = true
+			else:
+				$Tween.interpolate_property($FocalPoint/Cam, "zoom", $FocalPoint/Cam.zoom, narrow_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		7: #Global Map
+			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $CenterPoint.global_position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.interpolate_property($FocalPoint/Cam, "zoom", $FocalPoint/Cam.zoom, wide_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.start()
+			if reverse:
+				$HQ/Banks/Frankfurt.build(reverse)
+		8: #Zoom and Build Frankfurt
+			if !reverse:
+				$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $HQ/Banks/Frankfurt.global_position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+				$Tween.interpolate_property($FocalPoint/Cam, "zoom", $FocalPoint/Cam.zoom, narrow_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+				$Tween.start()
+				$HQ/Banks/Frankfurt.build()
+				$HQ/Banks/Frankfurt.Network = true
+			if reverse:
+				$HQ/Banks/London.Network = true
+				for edge in ldnEdges:
+					$HQ/Banks/Frankfurt/Edges.remove_child(edge)
+					$HQ/Banks/London/Edges.add_child(edge)
+					edge.position = edge.position - ($HQ/Banks/London/Edges.global_position - $HQ/Banks/Frankfurt/Edges.global_position)
+		9: #Disconnect London
+			if !reverse:
+				$HQ/Banks/London.Network = false
+				for edge in ldnEdges:
+					$HQ/Banks/London/Edges.remove_child(edge)
+					$HQ/Banks/Frankfurt/Edges.add_child(edge)
+					edge.position = edge.position + ($HQ/Banks/London/Edges.global_position - $HQ/Banks/Frankfurt/Edges.global_position)
+			if reverse:
+				$Tween.interpolate_property($FocalPoint/Cam, "zoom", $FocalPoint/Cam.zoom, narrow_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+				$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $HQ/Banks/Frankfurt.global_position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+				$Tween.start()
+		10: #Global View
+			$Tween.interpolate_property($FocalPoint, "position", $FocalPoint.position, $CenterPoint.position, move_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.interpolate_property($FocalPoint/Cam, "zoom", $FocalPoint/Cam.zoom, wide_zoom, zoom_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.start()
---
+++ leaderboard/src/main/Game.gd
@@ -0,0 +1,87 @@
+extends Node2D
+
+var ws = WebSocketClient.new()
+var _write_mode = WebSocketPeer.WRITE_MODE_TEXT
+var retryTimeout = 5 # seconds
+var wsUrlStr: String = JavaScript.eval("window.location.hostname+'/dashboard-socket'") if OS.has_feature('JavaScript') else "ws://ui-admin-hq.apps.summit-hq1.openshift.redhatkeynote.com/socket"
+var Lobby = preload("res://src/lobby/Lobby.tscn");
+var Leaderboard = preload("res://src/leaderboard/LeaderBoard.tscn");
+var Paused = preload("res://src/paused/Paused.tscn");
+var Stopped = preload("res://src/stopped/Stopped.tscn");
+
+var WS_URL = wsUrlStr if wsUrlStr.length() > 0 else "ws://ui-admin-hq.apps.summit-hq1.openshift.redhatkeynote.com/socket"
+
+func _init():
+	self._connect()
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	if ws.get_connection_status() == ws.CONNECTION_CONNECTING || ws.get_connection_status() == ws.CONNECTION_CONNECTED:
+		ws.poll()
+
+func _connect():
+	ws.connect("connection_established", self, "_connection_established")
+	ws.connect("connection_closed", self, "_connection_closed")
+	ws.connect("connection_error", self, "_connection_error")
+	ws.connect("data_received", self, "_handle_data_received")
+
+	print("Connecting to " + WS_URL)
+	ws.connect_to_url(WS_URL)
+
+func decode_data(data):
+	return data.get_string_from_utf8()
+
+func _handle_data_received():
+	var res = JSON.parse(decode_data(ws.get_peer(1).get_packet())).result
+	if 'game' in res:
+		if 'state' in res.game:
+			print('Game State: ',res.game.state)
+			showScene(res.game.state)
+			# lobby, active, paused, stopped
+
+func _connection_established(protocol):
+	ws.get_peer(1).set_write_mode(_write_mode)
+	send('{"type":"init"}')
+	print("Connection established with protocol: ", protocol)
+
+func _connection_closed(clean):
+	print("Connection closed, retrying in %ds" % retryTimeout)
+	$connection.start()
+
+func _connection_error():
+	print("Connection error, retrying in %ds" % retryTimeout)
+	$connection.start()
+
+func send(data):
+	ws.get_peer(1).set_write_mode(_write_mode)
+	ws.get_peer(1).put_packet(data.to_utf8())
+
+func _on_connection_timeout():
+	$connection.wait_time *= 2
+	self._connect()
+
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_ESCAPE:
+			get_tree().quit()
+
+func showScene(sceneName="Lobby"):
+	for sc in $Scenes.get_children():
+		if sc.name != sceneName:
+			sc.visible = false
+		else:
+			sc.visible = true
+
+func _on_Lobby_pressed():
+	showScene("Lobby")
+
+func _on_Game_pressed():
+	showScene("Leaderboard")
+
+func _on_Pause_pressed():
+	showScene("Paused")
+
+func _on_Stop_pressed():
+	showScene("Stopped")
